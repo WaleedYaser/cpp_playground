4cfdbef80a6e61f5a9de89908d5d1f70565de619
Waleed Yaser <waleedyaser95@gmail.com>
2019-01-22 21:22:12 +0200

    finish ch4 drill

--- ch4/drill.cpp
+++ ch4/drill.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 #include <float.h>
 
 int main()
 {
 	using namespace std;
 
-	double x;
-	double min = DBL_MAX, max = DBL_MIN;
-	while (cin >> x) {
-		cout << x << endl;
-		if (x < min) {
-			min = x;
-			cout << " the smallest so far";
+	double value;
+	string unit;
+	vector<double> values;
+
+	while (cin >> value >> unit) {
+		if (unit == "m")
+			values.push_back(value);
+		else if (unit == "cm")
+			values.push_back(value / 100);
+		else if (unit == "in")
+			values.push_back(value * 2.54 / 100);
+		else if (unit == "ft")
+			values.push_back(value * 2.54 * 12 / 100);
+		else {
+			cout << "illegal unit: " << unit << endl;
 		}
-		if (x > max) {
-			max = x;
-			cout << " the largest so far";
-		}
-		cout << endl;
 	}
+
+	sort(values.begin(), values.end());
+	for (const double &x : values)
+		cout << x << endl;
+
+	return 0;
 }
